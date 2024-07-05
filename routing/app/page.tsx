0ebbdf7a6305fafa30777f2af739d507b53b84f5
PRO0FESSOR <siddharth96549@gmail.com>
2024-07-05 18:16:42 +0530

    link component

--- routing/app/page.tsx
+++ routing/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link"
 
 const page = () => {
   return (
+    <>    
     <h1 className="flex justify-center">Hello, Next.js!</h1>
+    <Link className="border-red-600 hover:bg-red-500" href="/dashboard">Dashboard</Link>
+    </>
+    
+
   )
 }
 
