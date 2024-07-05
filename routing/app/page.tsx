07a9662ba7a0886ece5dc25d2f92bd284e0ce4d2
PRO0FESSOR <siddharth96549@gmail.com>
2024-07-05 22:38:03 +0530

    linking and navigation

--- routing/app/page.tsx
+++ routing/app/page.tsx
@@ -4,11 +4,28 @@ const page = () => {
   return (
     <>    
     <h1 className="flex justify-center">Hello, Next.js!</h1>
-    <Link className="border-red-600 hover:bg-red-500" href="/dashboard">Dashboard</Link>
-    </>
-    
-
+    <Link className="border-red-600 hover:bg-red-500" href="/dashboard" >Dashboard</Link>
+    </>  
   )
 }
+
+//use router - to change the routes from client components
+
+// 'use client'
+ 
+// import { useRouter } from 'next/navigation'
+ 
+// export default function Page() {
+//   const router = useRouter()
+ 
+//   return (
+//     <button type="button" onClick={() => router.push('/dashboard')}>
+//       Dashboard
+//     </button>
+//   )
+// }
+
+
+
 
 export default page
