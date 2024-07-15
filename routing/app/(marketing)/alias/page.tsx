2055d990f1630e97bb2ef2ee0697bf09cd19dfbc
PRO0FESSOR <siddharth96549@gmail.com>
2024-07-15 14:32:08 +0530

    next import alias

---
+++ routing/app/(marketing)/alias/page.tsx
@@ -0,0 +1,13 @@
+import Button from '@/app/components/Button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div>i am alias</div>
+    <Button/>
+    </>
+  )
+}
+
+export default page
